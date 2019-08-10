274866f54356fb9425d4d9b3da0b9f5173233bc2
Yogeros Smith <yogeross@buffalo.edu>
2019-08-09 21:06:00 -0400

    webpage update

--- src/main/webapp/TRMSdashboard.js
+++ src/main/webapp/TRMSdashboard.js
@@ -1,2 +1,22 @@
-var dashButtons= document.querySelectorAll(".dashBoardMenu .diffDashboards button");
-var tabcontent= document.querySelectorAll(".dashBoardMenu .tabContent button");
+function chooseTab() {
+    console.log("in choosetab");
+    alert("clicked");
+    //let tabcontent = document.getElementsByClassName("tabcontent");
+//     for (let i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     let tablinks = document.getElementsByClassName("tablink");
+//     for (let i = 0; i < tablinks.length; i++) {
+//       tablinks[i].style.backgroundColor = "";
+//     }
+//     document.getElementById(pageName).style.display = "block";
+//     elmnt.style.backgroundColor = color;
+   }
+  
+  
+  
+window.onload=function(){
+    console.log("in onload");
+//document.getElementById("defaultOpen").click();
+document.getElementsByClassName("tabs").add ("click",chooseTab);
+}    
