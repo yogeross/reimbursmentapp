aa12e574639d5dcfddcb94def7f68039bcbe8309
Andrew Valancius <avalancius6@gmail.com>
2019-08-11 17:05:21 -0400

    Fix requestdaoimpl function headers

--- src/main/java/com/ymba/dao/RequestDaoImpl.java
+++ src/main/java/com/ymba/dao/RequestDaoImpl.java
@@ -18,26 +18,8 @@ public class RequestDaoImpl {
 	
 	public static AwsConnection aws = AwsConnection.getInstance();
 	
-	/*
-	 * REATE TABLE request (
-    request_id NUMBER(10) PRIMARY KEY,
-    fk_employee_id NUMBER(10) NOT NULL REFERENCES employee(employee_id),
-    fk_event_id NUMBER(10) NOT NULL REFERENCES event(event_id),
-    date_submitted DATE NOT NULL,
-    status VARCHAR2(50) NOT NULL,
-    employee_comments VARCHAR2(300),
-    denial_reason VARCHAR2(300),
-    amount_approved NUMBER(10),
-    supervisor_id NUMBER(10) REFERENCES employee(employee_id),
-    supervisor_approval_date DATE,
-    dept_head_id NUMBER(10) REFERENCES employee(employee_id),
-    dept_head_approval_date DATE,
-    benco_id NUMBER(10) REFERENCES employee(employee_id),
-    benco_approval_date DATE
-
-	 */
 	
-	public static List<CurrentRequest> getAllRequests() throws SQLException {
+	public List<CurrentRequest> getAllRequests() throws SQLException {
 
 		List<CurrentRequest> requestList = new ArrayList<CurrentRequest>();
 		Connection conn = aws.getConnection();
@@ -53,7 +35,7 @@ public class RequestDaoImpl {
 	}
 
 	
-	public static List<CurrentRequest> getRequestsByEmployee(int employee_id) {
+	public List<CurrentRequest> getRequestsByEmployee(int employee_id) {
 		
 		Connection conn = aws.getConnection();
 		String sql = "SELECT * FROM REQUEST WHERE employee_id = ?";
