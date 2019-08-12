7e624bcac0b88d40738fdce030bc56bffcf47de7
Bradley Campbell <bradley@107-144-156-227.biz.spectrum.com>
2019-08-12 02:01:10 -0400

    Updates

--- src/main/webapp/js/TRMSdashboard.js
+++ src/main/webapp/js/TRMSdashboard.js
@@ -1,5 +1,60 @@
+function formDisplay() {
+    if (document.getElementById("reimbursementForm").style.display === "none") {
+        document.getElementById("reimbursementForm").style.display = "block";
+    } else {
+        document.getElementById("reimbursementForm").style.display = "none"
+    }
+}
 
+var today = new Date();
+var dd = String(today.getDate() + 7).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+week = yyyy + '-' + mm + '-' + dd;
 
+document.getElementById("date").addEventListener("change", function(){
+	var input = this.value;
+    if(input >= week){
+    	document.getElementById("errors").innerHTML = "";
+        document.getElementById("submitForm").disabled = false;
+    }else{
+    	document.getElementById("errors").innerHTML = "Events must start past " + week + " to be submitted.";
+        document.getElementById("submitForm").disabled = true;
+    }
+})
+
+function estimateCost(){
+	var eventType = document.getElementById("eventType");
+	var event = eventType.options[eventType.selectedIndex].value;
+    var cost= document.getElementById("cost").value;
+    var estimate=0;
+    switch (event){
+        case "1":
+       		estimate=cost;
+            document.getElementById("estimate").innerHTML="Estimated Cost we cover: $"+estimate.toString();
+            break;
+         case "2":
+            estimate=Math.round((cost*.9) * 100) / 100;
+            document.getElementById("estimate").innerHTML="Estimated Cost we cover: $"+estimate.toString();
+            break;
+        case "3":
+            estimate=Math.round((cost*.8) * 100) / 100;;
+            document.getElementById("estimate").innerHTML="Estimated Cost we cover: $"+estimate.toString();
+            break;
+        case "4":
+            estimate=Math.round((cost*.75) * 100) / 100;
+            document.getElementById("estimate").innerHTML="Estimated Cost we cover: $"+estimate.toString();
+            break;
+        case "5":
+            estimate=Math.round((cost*.6) * 100) / 100;
+            document.getElementById("estimate").innerHTML="Estimated Cost we cover: $"+estimate.toString();
+            break;
+        case "6":
+                estimate=Math.round((cost*.3) * 100) / 100;;
+                document.getElementById("estimate").innerHTML="Estimated Cost we cover: $"+cost.toString();
+                break;
+    }
+}
 
 function readCookie(name) {
     var nameEQ = name + "=";
@@ -12,211 +67,190 @@ function readCookie(name) {
     return null;
 }
 
-
-
-
-function logout(){
-    window.location.href="logout.html";
+function logout() {
+    window.location.href = "logout.html";
 }
 
-function buildRequestTable(request){
-    var table='';
-    let rows= request.length;
-    table+='<tr>';
-    table+='<th>' +"Event ID"+'</th>';
-    table+='<th>' +"Date Submitted"+'</th>';
-    table+='<th>' +"Status"+'</th>';
-    table+='<th>' +"Employee Comments"+'</th>';
-    table+='<th>' +"Denial Reason"+'</th>';
-    table+='<th>' +"Amount Approval"+'</th>';
-    table+='<th>' +"Supervisor ID"+'</th>';
-    table+='<th>' +"Supervisor Approval Date"+'</th>';
-    table+='<th>' +"Head ID"+'</th>';
-    table+='<th>' +"Head Approval Date"+'</th>';
-    table+='<th>' +"Benco ID"+'</th>';
-    table+='<th>' +"Benco Approval Date"+'</th>';
-    table+='</tr>';
-    for (let r=0;r<rows;r++){
-        table+='<tr>';
-        table=loadPastRequests(request[r],table);
-        table+='</tr>';
+function buildRequestTable(request) {
+    var table = '';
+    let rows = request.length;
+    table += '<tr>';
+    table += '<th>' + "Event ID" + '</th>';
+    table += '<th>' + "Date Submitted" + '</th>';
+    table += '<th>' + "Status" + '</th>';
+    table += '<th>' + "Employee Comments" + '</th>';
+    table += '<th>' + "Denial Reason" + '</th>';
+    table += '<th>' + "Amount Approval" + '</th>';
+    table += '<th>' + "Supervisor ID" + '</th>';
+    table += '<th>' + "Supervisor Approval Date" + '</th>';
+    table += '<th>' + "Head ID" + '</th>';
+    table += '<th>' + "Head Approval Date" + '</th>';
+    table += '<th>' + "Benco ID" + '</th>';
+    table += '<th>' + "Benco Approval Date" + '</th>';
+    table += '</tr>';
+    for (let r = 0; r < rows; r++) {
+        table += '<tr>';
+        table = loadPastRequests(request[r], table);
+        table += '</tr>';
     }
-    document.getElementById("requests").insertAdjacentHTML('beforeend','<table border=1>'+table+'</table>');
-    
+    document.getElementById("requests").insertAdjacentHTML('beforeend', '<table border=1>' + table + '</table>');
+
 }
-function loadPastRequests(request, table){
-    table+='<td>'+request.event_id+'</td>';
-    table+='<td>' +request.date+'</td>';
-    table+='<td>' +request.status+'</td>';
-    table+='<td>' +request.empComments+'</td>';
-    table+='<td>' +request.denialReason+'</td>';
-    table+='<td>' +request.amountApproval+'</td>';
-    table+='<td>' +request.supeID+'</td>';
-    table+='<td>' +request.supeApprovalDate+'</td>';
-    table+='<td>' +request.headID+'</td>';
-    table+='<td>' +request.headApprovalDate+'</td>';
-    table+='<td>' +request.bencoID+'</td>';
-    table+='<td>' +request.bencoApprovalDate+'</td>';
+function loadPastRequests(request, table) {
+    table += '<td>' + request.event_id + '</td>';
+    table += '<td>' + request.date + '</td>';
+    table += '<td>' + request.status + '</td>';
+    table += '<td>' + request.empComments + '</td>';
+    table += '<td>' + request.denialReason + '</td>';
+    table += '<td>' + request.amountApproval + '</td>';
+    table += '<td>' + request.supeID + '</td>';
+    table += '<td>' + request.supeApprovalDate + '</td>';
+    table += '<td>' + request.headID + '</td>';
+    table += '<td>' + request.headApprovalDate + '</td>';
+    table += '<td>' + request.bencoID + '</td>';
+    table += '<td>' + request.bencoApprovalDate + '</td>';
     return table;
 }
 
 
 //supervisor, head of department request table
-function buildSupervisorAndHeadRequestTable(request){
-    var table='';
+function buildSupervisorAndHeadRequestTable(request) {
+    var table = '';
     let rows = request.length;
-    table+='<tr>';
-    table+='<th>'+"Request ID"+'</th>';
-    table+='<th>' +"Event ID"+'</th>';
-    table+='<th>' +"Date Submitted"+'</th>';
-    table+='<th>' +"Status"+'</th>';
-    table+='<th>' +"Employee Comments"+'</th>';
-    table+='<th>' +"Denial Reason"+'</th>';
-    table+='<th>' +"Amount Approval"+'</th>';
-    table+='<th>'+"Request Options"+'</th>';
-    table+='</tr>';
-    for(let r=0; r<rows;r++){
-        table+='<tr>';
-        table=loadPastRequestsForSupervisorAndHead(request[r],table);
-        table+='</tr>';
+    table += '<tr>';
+    table += '<th>' + "Request ID" + '</th>';
+    table += '<th>' + "Event ID" + '</th>';
+    table += '<th>' + "Date Submitted" + '</th>';
+    table += '<th>' + "Status" + '</th>';
+    table += '<th>' + "Employee Comments" + '</th>';
+    table += '<th>' + "Denial Reason" + '</th>';
+    table += '<th>' + "Amount Approval" + '</th>';
+    table += '<th>' + "Request Options" + '</th>';
+    table += '</tr>';
+    for (let r = 0; r < rows; r++) {
+        table += '<tr>';
+        table = loadPastRequestsForSupervisorAndHead(request[r], table);
+        table += '</tr>';
     }
-    document.getElementById("incomingRequests").insertAdjacentHTML('beforeend','<table border=1>'+table+'</table>');
+    document.getElementById("incomingRequests").insertAdjacentHTML('beforeend', '<table border=1>' + table + '</table>');
 
 }
-function buildBencoTable(request){
-    var table='';
+function buildBencoTable(request) {
+    var table = '';
     let rows = request.length;
-    table+='<tr>';
-    table+='<th>'+"Request ID"+'</th>';
-    table+='<th>' +"Event ID"+'</th>';
-    table+='<th>' +"Date Submitted"+'</th>';
-    table+='<th>' +"Status"+'</th>';
-    table+='<th>' +"Employee Comments"+'</th>';
-    table+='<th>' +"Denial Reason"+'</th>';
-    table+='<th>' +"Amount Approval"+'</th>';
-    table+='<th>'+"Request Options"+'</th>';
-    table+='</tr>';
-    for(let r=0; r<rows;r++){
-        table+='<tr>';
-        table=loadPastRequestsForBenco(request[r],table);
-        table+='</tr>';
+    table += '<tr>';
+    table += '<th>' + "Request ID" + '</th>';
+    table += '<th>' + "Event ID" + '</th>';
+    table += '<th>' + "Date Submitted" + '</th>';
+    table += '<th>' + "Status" + '</th>';
+    table += '<th>' + "Employee Comments" + '</th>';
+    table += '<th>' + "Denial Reason" + '</th>';
+    table += '<th>' + "Amount Approval" + '</th>';
+    table += '<th>' + "Request Options" + '</th>';
+    table += '</tr>';
+    for (let r = 0; r < rows; r++) {
+        table += '<tr>';
+        table = loadPastRequestsForBenco(request[r], table);
+        table += '</tr>';
     }
-    document.getElementById("incomingRequestsForBenco").insertAdjacentHTML('beforeend','<table border=1>'+table+'</table>');
+    document.getElementById("incomingRequestsForBenco").insertAdjacentHTML('beforeend', '<table border=1>' + table + '</table>');
 
 }
 
-function loadPastRequestsForBenco(request,table){
-    table+='<td>'+request.request_id+'</td>';
-    table+='<td>'+request.event_id+'</td>';
-    table+='<td>' +request.date+'</td>';
-    table+='<td>' +request.status+'</td>';
-    table+='<td>' +request.empComments+'</td>';
-    table+='<td>' +request.denialReason+'</td>';
-    table+='<td>' +request.amountApproval+'</td>';
-    table+='<td>'+'<form method="POST"><input type="hidden" name="requuestID" value="'+request.request_id>+'"><button type="submit" formaction="approve-reimbursment">Approve</button>'+
-    '<button type="submit" formaction="deny-reimbursment">Deny</button>'+
-    '<button type="submit" formaction="requestInfo-reimbursment">Request Info</button>'+'</form>'+'</td>';
+function loadPastRequestsForBenco(request, table) {
+    table += '<td>' + request.request_id + '</td>';
+    table += '<td>' + request.event_id + '</td>';
+    table += '<td>' + request.date + '</td>';
+    table += '<td>' + request.status + '</td>';
+    table += '<td>' + request.empComments + '</td>';
+    table += '<td>' + request.denialReason + '</td>';
+    table += '<td>' + request.amountApproval + '</td>';
+    table += '<td>' + '<form method="POST"><input type="hidden" name="requuestID" value="' + request.request_id > +'"><button type="submit" formaction="approve-reimbursment">Approve</button>' +
+        '<button type="submit" formaction="deny-reimbursment">Deny</button>' +
+        '<button type="submit" formaction="requestInfo-reimbursment">Request Info</button>' + '</form>' + '</td>';
     return table
 }
-function loadPastRequestsForSupervisorAndHead(request,table){
-    table+='<td >'+request.request_id+'</td>';
-    table+='<td>'+request.event_id+'</td>';
-    table+='<td>' +request.date+'</td>';
-    table+='<td>' +request.status+'</td>';
-    table+='<td>' +request.empComments+'</td>';
-    table+='<td>' +request.denialReason+'</td>';
-    table+='<td>' +request.amountApproval+'</td>';
-    table+='<td>'+'<form method="POST"><input type="hidden" name="requuestID" value="'+request.request_id>+'"><button type="submit" formaction="approve-reimbursment">Approve</button>'+
-           '<button type="submit" formaction="deny-reimbursment">Deny</button>'+
-           '<button type="submit" formaction="requestInfo-reimbursment">Request Info</button>'+'</form>'+'</td>';
+function loadPastRequestsForSupervisorAndHead(request, table) {
+    table += '<td >' + request.request_id + '</td>';
+    table += '<td>' + request.event_id + '</td>';
+    table += '<td>' + request.date + '</td>';
+    table += '<td>' + request.status + '</td>';
+    table += '<td>' + request.empComments + '</td>';
+    table += '<td>' + request.denialReason + '</td>';
+    table += '<td>' + request.amountApproval + '</td>';
+    table += '<td>' + '<form method="POST"><input type="hidden" name="requuestID" value="' + request.request_id > +'"><button type="submit" formaction="approve-reimbursment">Approve</button>' +
+        '<button type="submit" formaction="deny-reimbursment">Deny</button>' +
+        '<button type="submit" formaction="requestInfo-reimbursment">Request Info</button>' + '</form>' + '</td>';
     return table
 }
 
-function getIncomingRequestsForSupervisorAndHead(){
-	let username= readCookie("username");
-	console.log("TEST");
+function getIncomingRequestsForSupervisorAndHead() {
+    let username = readCookie("username");
+    console.log("TEST");
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange=function(){
-        if(xhr.readyState==4 && xhr.status==200){
-            var request =JSON.parse(xhr.responseText);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var request = JSON.parse(xhr.responseText);
             console.log(typeof request);
             console.log("Printing request to log");
             console.log(request);
             buildSupervisorAndHeadRequestTable(request);
         }
     }
-    xhr.open("GET","http://localhost:8080/Project1/list-reimbursements?username="+username,true);
+    xhr.open("GET", "http://localhost:8080/Project1/list-reimbursements?username=" + username, true);
     //xhr.open("GET","http://localhost:8080/Project1/list-reimbursements?username=MHara5",true);
     xhr.send();
 
 }
 
-function getIncomingRequestsForBenco(){
-	let username= readCookie("username");
-	console.log("TEST");
+function getIncomingRequestsForBenco() {
+    let username = readCookie("username");
+    console.log("TEST");
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange=function(){
-        if(xhr.readyState==4 && xhr.status==200){
-            var request =JSON.parse(xhr.responseText);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var request = JSON.parse(xhr.responseText);
             console.log(typeof request);
             console.log("Printing request to log");
             console.log(request);
             buildBencoTable(request);
         }
     }
-    xhr.open("GET","http://localhost:8080/Project1/list-reimbursements?username="+username,true);
+    xhr.open("GET", "http://localhost:8080/Project1/list-reimbursements?username=" + username, true);
     //xhr.open("GET","http://localhost:8080/Project1/list-reimbursements?username=MHara5",true);
     xhr.send();
 
 }
 
-function getPastRequests(){
-	let username= readCookie("username");
-	console.log("TEST");
+function getPastRequests() {
+    let username = readCookie("username");
+    console.log("TEST");
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange=function(){
-        if(xhr.readyState==4 && xhr.status==200){
-            var request =JSON.parse(xhr.responseText);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var request = JSON.parse(xhr.responseText);
             console.log(typeof request);
             console.log("Printing request to log");
             console.log(request);
             buildRequestTable(request);
         }
     }
-    xhr.open("GET","http://localhost:8080/Project1/list-reimbursements?username="+username,true);
+    xhr.open("GET", "http://localhost:8080/Project1/list-reimbursements?username=" + username, true);
     //xhr.open("GET","http://localhost:8080/Project1/list-reimbursements?username=MHara5",true);
     xhr.send();
 
 }
 
 
-
-function lateSubmission(event){
-    let curDate =  new Date();
-    curDate.setHours(0,0,0,0); //Set to midnight
- 
-    let userDate = document.getElementById("date").value;
-    console.log("LATE SUBMISSION");
-    userDate = userDate.split("-");
-    userDate = new Date(userDate[0], userDate[1]-1, userDate[2], 0, 0, 0); //Months are zero-indexed
- 
-    if (userDate-curDate <= 86400000*7) {//ms per day; 7 days before
-      document.getElementById("submitForm").disabled=true;
-    }
-    else{
-        alert("You cannot submit a request less than 1 week from now");
-      document.getElementById("submitForm").disabled=false; //reenable in case it was disabled before
-    }}
-
-window.onload = function(){
-	console.log("ON LOAD");
-	console.log(document.cookie);
+window.onload = function () {
+    console.log("ON LOAD");
+    console.log(document.cookie);
     getPastRequests();
     getIncomingRequestsForSupervisorAndHead();
     getIncomingRequestsForBenco();
 };
 
-document.querySelector("#date").addEventListener("change",lateSubmission,true);
+document.querySelector("#date").addEventListener("change", lateSubmission, true);
 
 
 
