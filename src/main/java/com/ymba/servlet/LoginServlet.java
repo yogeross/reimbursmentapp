68ee2f7272b1ef40642047ee5b9448c7c25ccaee
Andrew Valancius <avalancius6@gmail.com>
2019-08-11 21:30:53 -0400

    Fix dashboard.html version

--- src/main/java/com/ymba/servlet/LoginServlet.java
+++ src/main/java/com/ymba/servlet/LoginServlet.java
@@ -22,7 +22,7 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		request.getRequestDispatcher("login.html").forward(request, response);
+		request.getRequestDispatcher("index.html").forward(request, response);
 
 		
 	
